3df67196f0bd5655661e710d63c04c846c7c9ca0
chlohal <coleh@coleh.net>
2022-09-12 11:15:39 -0400

    Remove a debug print

--- TeamCode/src/main/scripts/autoauto-compiler/compiler/transmutations/process-template/template-filename.js
+++ TeamCode/src/main/scripts/autoauto-compiler/compiler/transmutations/process-template/template-filename.js
@@ -4,6 +4,4 @@ const { existsSync } = require("fs");
 var path = require("path");
 const commandLineInterface = require("../../../../command-line-interface");
 
-console.log(commandLineInterface);
-
 module.exports = path.resolve(".", commandLineInterface["output-java-template"]);
