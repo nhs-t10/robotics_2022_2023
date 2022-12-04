db4e9c54e26320301c4361aeea9ccd4129d1d5d0
achyutS1281 <achyut.shastri@gmail.com>
2022-12-04 12:01:27 -0500

    Edited the Javadocs and redid the toString method of RRManager'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/roadrunner/RRManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/roadrunner/RRManager.java
@@ -14,9 +14,12 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuild
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.*;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
- * Manager for Pathing and Dead Reckoning... Makes Road runner much easier to use ith a set of complex methods for making precise paths. created by ACHYUT SHASTRI
+ * Manager for Pathing and Dead Reckoning... Makes Road runner much easier to use with a set of complex methods for making precise paths. created by ACHYUT SHASTRI
  */
 public class RRManager extends FeatureManager {
     private SampleMecanumDrive driveRR;
@@ -29,6 +32,8 @@ public class RRManager extends FeatureManager {
      * @param hardwareMap The hardwareMap for Roadrunner to access for the drive motors
      * @param start The start position for the robot
      * @param telemetryManager The telemetry manager to use for telemetry logging
+     * {@link #telemetry}
+     *
      */
     public RRManager(@NotNull HardwareMap hardwareMap, @NotNull Pose2d start, @NotNull TelemetryManager telemetryManager){
         driveRR = new SampleMecanumDrive(hardwareMap); //Necessary Component for RoadRunner!
@@ -36,7 +41,7 @@ public class RRManager extends FeatureManager {
 
         this.telemetry = telemetryManager;
         calibrateDriveToZero();
-        telemetry.addLine("Go to 192.168.43.1:8080/dash for the FTC Dashboard! Unless this is the competition, for which, in that case, never mind...");
+        telemetry.log().add("Go to 192.168.43.1:8080/dash for the FTC Dashboard! Unless this is the competition, for which, in that case, never mind, don't use FTC Dashboard...");
 
     }
 
@@ -86,7 +91,7 @@ public class RRManager extends FeatureManager {
     public void calibrateDriveToZero(){
         driveRR.setPoseEstimate(new Pose2d(0, 0));
 
-        telemetry.addLine("RoadRunner Drive Recalibrated");
+        telemetry.log().add("RoadRunner Drive Recalibrated");
     }
 
     /**
@@ -95,7 +100,7 @@ public class RRManager extends FeatureManager {
 
     @TestOnly public void calibrateTrajectoryBuilderToZero(){
         trajBuildRR = driveRR.trajectoryBuilder(new Pose2d(0, 0));
-        telemetry.addLine("RoadRunner Drive Recalibrated");
+        telemetry.log().add("RoadRunner Drive Recalibrated");
     }
 
     /**
@@ -127,7 +132,7 @@ public class RRManager extends FeatureManager {
         }
         else if(type.equals("splineline")){
             driveRR.followTrajectory(trajBuildRR.splineToLinearHeading(pose, Math.toRadians(rotation)).build());
-            telemetry.addLine("WARNING! Using this movement will likely result in a PathContinuityError!");
+            telemetry.log().add("WARNING! Using this movement will likely result in a PathContinuityError!");
         }
         updateLocalizer();
     }
@@ -167,7 +172,7 @@ public class RRManager extends FeatureManager {
                 driveRR.followTrajectory(trajBuildRR.splineToSplineHeading(pose, Math.toRadians(rotation)).build());
             } else if (type.equals("splineline")) {
                 driveRR.followTrajectory(trajBuildRR.splineToLinearHeading(pose, Math.toRadians(rotation)).build());
-                telemetry.addLine("WARNING! Using this movement will likely result in a PathContinuityError!");
+                telemetry.log().add("WARNING! Using this movement will likely result in a PathContinuityError!");
             }
 
             driveRR.waitForIdle();
@@ -183,7 +188,7 @@ public class RRManager extends FeatureManager {
      * @throws SequenceInitException The exception that tells you a sequence calculation has filed on init
      * @throws Exception The regular exception that SequenceInit Exception uses
      */
-
+    @Test
     public void customMoveSequenceWithPoseTrajSequence(@NotNull Pose2d[] poseArr, @NotNull String[] typeArr, @NotNull double[] rotationArr) throws SequenceInitException, Exception {
         if(poseArr.length != typeArr.length || typeArr.length != rotationArr.length || poseArr.length != rotationArr.length){
             throw new SequenceInitException("Array Lengths for sequence do not match! "+poseArr.length+" does not equal "+typeArr.length+" or does not equal "+rotationArr.length);
@@ -212,7 +217,7 @@ public class RRManager extends FeatureManager {
                 tsb = tsb.splineToSplineHeading(pose, Math.toRadians(rotation));
             } else if (type.equals("splineline")) {
                 tsb = tsb.splineToLinearHeading(pose, Math.toRadians(rotation));
-                telemetry.addLine("WARNING! Using this movement will likely result in a PathContinuityError!");
+                telemetry.log().add("WARNING! Using this movement will likely result in a PathContinuityError!");
             }
             driveRR.followTrajectorySequence(tsb.build());
             driveRR.waitForIdle();
@@ -245,15 +250,6 @@ public class RRManager extends FeatureManager {
     }
 
     /**
-     * Returns teh overview of Roadrunner's status
-     * @return String of status
-     */
-    @Override
-    public String toString(){
-        return "Road Runner with position of "+getPose().toString();
-    }
-
-    /**
      * Determines whether a pose is viable for te robot to go to
      * @param pose2d The pose to test
      * @return Boolean on whether the pose is viable
@@ -269,5 +265,21 @@ public class RRManager extends FeatureManager {
         }
         telemetry.log().add("Path Accepted");
         return true;
+    }
+
+    /**
+     * Returns the overview of Roadrunner's instance variables and reference ids
+     *
+     * @return String of overview
+     */
+    @Override
+    public String toString() {
+        return "RRManager{" +
+                "driveRR=" + driveRR +
+                ", trajBuildRR=" + trajBuildRR +
+                ", tsb=" + tsb +
+                ", telemetry=" + telemetry +
+                ", nonono=" + Arrays.toString(nonono) +
+                '}';
     }
 }
