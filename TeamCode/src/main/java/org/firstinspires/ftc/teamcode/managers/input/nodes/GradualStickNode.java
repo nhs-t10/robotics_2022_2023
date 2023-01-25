16791a9977eecdbf2a35db0efebb359246f83864
Mario-Runner <73408352+Mario-Runner@users.noreply.github.com>
2023-01-24 19:38:46 -0500

    Fixed Gradual Stick Node

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/input/nodes/GradualStickNode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/input/nodes/GradualStickNode.java
@@ -24,7 +24,7 @@ public class GradualStickNode extends InputManagerInputNode{
     private long accelerationStartTime;
 
     /**
-     * Gradually accelerates an stick input over a given amount of time (in milliseconds). <br>
+     * Gradually accelerates an stick input by a certain amount of speed per frame <br>
      * If the starting speed value is greater than the ending speed value, this can be used to gradually slow down as well. <br>
      * When the input is false, the node will instantly reset to the default value, and it will <i>not</i> return gradually.<br>
      *
@@ -32,7 +32,7 @@ public class GradualStickNode extends InputManagerInputNode{
      *
      * @param control The input that will be accelerated
      * @param initialSpeed The initial speed of the input
-     * @param accelConstant How much the speed will increase per frame
+     * @param accelConstant How much the speed will increase per frame. NOTE: This number must be very small, 0.0001f as an example, since it adds this speed every tick. Too high and the acceleration will be nearly insteant.
      * @see AccelerationNode#AccelerationNode(InputManagerInputNode, InputManagerInputNode, InputManagerInputNode, InputManagerInputNode) AccelerationNode
      * @see DecelerationNode#DecelerationNode(InputManagerInputNode, InputManagerInputNode, InputManagerInputNode, InputManagerInputNode) DecelerationNode
      * @see BothcelerationNode#BothcelerationNode(InputManagerInputNode, InputManagerInputNode, InputManagerInputNode, InputManagerInputNode) BothcelerationNode
@@ -51,28 +51,31 @@ public class GradualStickNode extends InputManagerInputNode{
     @Override
     public void update() {
         control.update();
-        isActive = Math.abs(control.getResult().getFloat()) >= startingSpeed;
+        isActive = Math.abs(control.getResult().getFloat()) >= 0.1f;
         endingSpeed = control.getResult().getFloat();
-
-        if(isActive && !wasActive) {
-            //accelerationStartTime = RobotTime.currentTimeMillis();
-            currentSpeed = startingSpeed;
-        }
         if(isActive) {
+            if(!wasActive) {
+                if (endingSpeed > 0) {
+                    currentSpeed = startingSpeed;
+                }
+                else {
+                    currentSpeed = -startingSpeed;
+                }
+            }
             //Old implementation
             //long timeSinceStart = RobotTime.currentTimeMillis() - accelerationStartTime;
             //float percentageCompleted = Math.min(1, timeSinceStart / movementTime);
             //resultNumber = startingSpeed + percentageCompleted * (endingSpeed - startingSpeed);
             if (currentSpeed > 0) {
                 if ((currentSpeed + accelerationConstant) < endingSpeed) {
-                    currentSpeed += accelerationConstant;
+                    currentSpeed = currentSpeed + accelerationConstant;
                 } else {
                     currentSpeed = endingSpeed;
                 }
             }
             else {
                 if ((currentSpeed - accelerationConstant) > endingSpeed) {
-                    currentSpeed -= accelerationConstant;
+                    currentSpeed = currentSpeed - accelerationConstant;
                 } else {
                     currentSpeed = endingSpeed;
                 }
